3d085526515bd7d8e945e071df69f034216630a3
COBI <francescoobiondi77@gmail.com>
2026-06-11 13:09:22 +0200

    Update touchpad gestures for scrolling layout

--- hypr/.config/hypr/lua/50-input.lua
+++ hypr/.config/hypr/lua/50-input.lua
@@ -21,8 +21,27 @@ hl.config({
 
 hl.gesture({
   fingers = 3,
-  direction = "horizontal",
+  direction = "vertical",
   action = "workspace",
+  scale = 1.5,
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "scroll_move",
+    scale = 1.5,
+})
+
+hl.gesture({
+    fingers = 4,
+    direction = "vertical",
+    action = function()
+        if hl.plugin and hl.plugin.scrolloverview then
+            hl.plugin.scrolloverview.overview("toggle")
+        end
+    end,
+    scale = 1.5,
 })
 
 hl.device({
